7f20c6a4420b4b96605f71f0aed9d3bb1f9d08a9
Jxavier <jxavierpaul.dev@proton.me>
2024-12-12 15:43:39 -0400

    init: basic window

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[]) {
+  QGuiApplication app(argc, argv);
+
+  QQmlApplicationEngine engine;
+  using namespace Qt::StringLiterals;
+  const QUrl url(u"qrc:/Uwide/main.qml"_s);
+  QObject::connect(
+      &engine, &QQmlApplicationEngine::objectCreated, &app,
+      [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+          QCoreApplication::exit(-1);
+      },
+      Qt::QueuedConnection);
+  engine.load(url);
+
+  return app.exec();
+}
